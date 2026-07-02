be6715be632d42b123cfff7f0d0e9553743b9ab1
joaopaulomarasca <joao_tessaro@estudante.sesisenai.org.br>
2026-07-02 20:45:15 -0300

    Atividade 2: sistema de boas vindas

---
+++ node.js/atividade2.js
@@ -0,0 +1,14 @@
+function ola(){
+     console.log("olá bem vindo")
+};
+
+function tchau(){
+    console.log("Tcahu volte sempre")
+};
+
+let autorSistema = "Administrador"; 
+
+module.exports ={
+    ola,
+    tchau,
+}
