83f92dc9a5355def7a06ab503db17fbe56151470
joaopaulomarasca <joao_tessaro@estudante.sesisenai.org.br>
2026-07-02 19:51:20 -0300

    Atividade 1: Criar uma calculadora simples

---
+++ node.js/atividade1.js
@@ -0,0 +1,21 @@
+function somar(a, b){
+    return a + b;
+}
+function menos(a,b){
+    return a - b;
+}
+
+function vezes(a,b){
+    return a * b;
+}
+
+function divisao(a,b){
+    return a / b;
+}
+
+module.exports ={
+    somar,
+    menos,
+    vezes,
+    divisao,
+};
---
+++ node.js/calculadora.js
@@ -0,0 +1,5 @@
+const calculadora = require('./atividade1');
+console.log("Soma:", calculadora.somar(10, 5));
+console.log("Subtração:", calculadora.menos(10, 5));
+console.log("Multiplicação:", calculadora.vezes(10, 5));
+console.log("Divisão:", calculadora.divisao(10, 5));
