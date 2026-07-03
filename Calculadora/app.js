9a9ae0bc66d9b79213850bdf2ee7bcc404e31e38
joaopaulomarasca <joao_tessaro@estudante.sesisenai.org.br>
2026-07-02 22:02:29 -0300

    Atividade de fazer uma calculadora quase concluida

---
+++ Calculadora/app.js
@@ -0,0 +1,6 @@
+const calculadora = require('./Operacoes');
+const calculadora = require('./Validacao');
+console.log("Soma:", calculadora.somar(10, 5));
+console.log("Subtração:", calculadora.menos(10, 5));
+console.log("Multiplicação:", calculadora.vezes(10, 5));
+console.log("Divisão:", calculadora.divisao(10, 5));
---
+++ Calculadora/utils/Validacao.js
@@ -0,0 +1,10 @@
+function Numero(numero) {
+    if (numero.length >= 2) {
+        return numero;
+    } else {
+        return;
+    }
+}
+module.exports = {
+    numero,
+};
